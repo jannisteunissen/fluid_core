15c4a03518fea823b8da5160daa9535d77af191b
jannisteunissen <jannis@teunissen.net>
2014-05-01 15:52:55 +0200

    Simplified code, still need to test and adapt

--- m_transport_data.f90
+++ m_transport_data.f90
@@ -6,44 +6,29 @@ module m_transport_data
    private
 
    integer, parameter :: dp = kind(0.0d0)
-   integer, parameter :: max_num_rows = 200 !> The maximum number of rows of transport data for an entry
-   integer, parameter :: TD_nEntriesMax = 100   !> The maximum number of transport data entries
-   integer, parameter :: nameLen = 40, lineLen = 100
+   integer, parameter :: max_num_rows   = 200 !> The maximum number of rows of transport data for an entry
+   integer, parameter :: nameLen        = 40
+   integer, parameter :: lineLen        = 100
 
-   type TD_table_type
-      real(dp), pointer      :: TD(:,:)
-      integer                :: n_rows
-      character(LEN=nameLen) :: gas_name
-      character(LEN=nameLen) :: data_name
-      character(LEN=lineLen) :: comment
-   end type TD_table_type
-
-   !> The number of transport data entries read in
-   integer :: TD_nEntries = 0
-
-   !> The actual table for storing transport data, could be allocated
-   !! dynamically in the future
-   type(TD_table_type) :: TD_table(TD_nEntriesMax)
-
-   public :: TD_read_file
-   public :: TD_get_data
+   public :: TD_get_td_from_file
 
 contains
 
    !> Routine to read in transport data from a file
-   !! Searches 'filename' for transport data concerning 'gas_name'
-   subroutine TD_read_file(filename, gas_name)
-      character(LEN=*), intent(IN) :: filename, gas_name
+   !! Searches 'file_name' for transport 'data_name' concerning 'gas_name'
+   function TD_get_td_from_file(file_name, data_name, gas_name, x_data, y_data)
+      character(len=*), intent(in)         :: file_name, data_name, gas_name
+      real(dp), allocatable, intent(inout) :: x_data(:), y_data(:)
 
       ! Temporary variables
-      integer                      :: gas_name_len
-      integer                      :: dIx
-      integer                      :: ioState, nL
-      integer                      :: n_rows
-      integer, parameter           :: my_unit = 333
-      character(LEN=nameLen)       :: line_fmt, data_name
-      character(LEN=lineLen)       :: line, prev_line
-      real(dp)             :: tempArray(2, max_num_rows)
+      integer                :: gas_name_len
+      integer                :: dIx
+      integer                :: ioState, nL
+      integer                :: n_rows
+      integer, parameter     :: my_unit = 333
+      character(LEN=nameLen) :: line_fmt, cur_name
+      character(LEN=lineLen) :: line, prev_line
+      real(dp)               :: tempArray(2, max_num_rows)
 
       nL = 0     ! Set the number of lines to 0
       gas_name_len = len(gas_name)
@@ -52,8 +37,8 @@ contains
       write(line_fmt, FMT = "(I6)") lineLen
       line_fmt = "(A" // trim(adjustl(line_fmt)) // ")"
 
-      ! Open 'filename' (with error checking)
-      open(my_unit, FILE = filename, STATUS = "OLD", ACTION = "READ", ERR = 999, IOSTAT = ioState)
+      ! Open 'file_name' (with error checking)
+      open(my_unit, FILE = file_name, STATUS = "OLD", ACTION = "READ", ERR = 999, IOSTAT = ioState)
 
       ! Look for collision processes with the correct gas name in the file,
       ! which should contains entries like below:
@@ -80,28 +65,14 @@ contains
          end do
 
          ! prev_line holds the type of transport data, see the formatting above
-         data_name = trim(prev_line)
-
-         ! Update the number of processes and set the gas name and data name
-         TD_nEntries = TD_nEntries + 1
-         dIx = TD_nEntries
-
-         if (TD_nEntries > TD_nEntriesMax) then
-            print *, "TD_read_file error, too many transport data entries"
-            return
-         end if
-
-         TD_table(dIx)%gas_name  = gas_name
-         TD_table(dIx)%data_name = data_name
+         if (data_name /= trim(prev_line)) cycle
 
          ! Now we can check whether there is a comment, while scanning lines until dashes are found,
          ! which indicate the start of the transport data
          do
             read(my_unit, FMT = line_fmt, ERR = 999, end = 555) line; nL = nL+1
             line = adjustl(line)
-            if ( line(1:8) == "COMMENT:" ) then
-               TD_table(dIx)%comment = adjustl( line(10:) )
-            end if
+            if ( line(1:8) == "COMMENT:" ) cycle ! Do nothing for now
             if ( line(1:5) == "-----" ) exit
          end do
 
@@ -118,65 +89,39 @@ contains
                n_rows = n_rows + 1
                read(line, FMT = *, ERR = 999, end = 555) tempArray(:, n_rows)
             else
-               print *, "CS_read_file error: too many rows in ", filename, " at line ", nL
+               print *, "CS_read_file error: too many rows in ", file_name, " at line ", nL
             end if
          end do
 
          ! Store the data in the actual table
-         allocate( TD_table(dIx)%TD(2, n_rows) )
-         TD_table(dIx)%n_rows = n_rows
-         TD_table(dIx)%TD = tempArray(:, 1:n_rows)
+         if (allocated(x_out)) deallocate(x_out)
+         if (allocated(y_out)) deallocate(y_out)
+         allocate(x_out(n_rows))
+         allocate(y_out(n_rows))
 
+         x_out = tempArray(1, 1:n_rows)
+         y_out = tempArray(2, 1:n_rows)
+
+         exit                   ! Done
       end do
 
-555   continue ! Routine ends here if the end of "filename" is reached erroneously
-      print *, "TD_read_file error, reached end of file while searching:"
-      print *, "ioState = ", ioState, " while reading from [", filename, "] at line ", nL
+555   continue ! Routine ends here if the end of "file_name" is reached erroneously
+      print *, "TD_get_td_from_file error, reached end of file while searching:"
+      print *, "ioState = ", ioState, " while reading from [", file_name, "] at line ", nL
       close(my_unit, ERR = 999, IOSTAT = ioState)
       return
 
 
-666   continue ! Routine ends here if the end of "filename" is reached correctly
+666   continue ! Routine ends here if the end of "file_name" is reached correctly
       close(my_unit, ERR = 999, IOSTAT = ioState)
       return
 
 
 999   continue ! If there was an error, the routine will end here
-      print *, "TD_read_file error at line", nL
-      print *, "ioState = ", ioState, " while searching [", gas_name, "] in [", filename, "]"
+      print *, "TD_get_td_from_file error at line", nL
+      print *, "ioState = ", ioState, " while searching [", gas_name, "] in [", file_name, "]"
       stop
 
-
-   end subroutine TD_read_file
-
-   !> Get the transport data with name 'data_name'
-   subroutine TD_get_data(data_name, x_out, y_out)
-      character(len=*), intent(in)         :: data_name
-      real(dp), allocatable, intent(inout) :: x_out(:), y_out(:)
-      integer                              :: n, dIx
-
-      dIx = -1    ! Find the transport data corresponding to data_name
-      do n = 1, TD_nEntries
-         if (TD_table(n)%data_name == data_name) then
-            dIx = n
-            exit
-         end if
-      end do
-
-      if (dIx == -1) then
-         print *, "TD_get_data error, data with name ", data_name
-         print *, "could not be found!"
-         stop
-      end if
-
-      if (allocated(x_out)) deallocate(x_out)
-      if (allocated(y_out)) deallocate(y_out)
-      allocate( x_out(TD_table(dIx)%n_rows) )
-      allocate( y_out(TD_table(dIx)%n_rows) )
-
-      x_out = TD_table(dIx)%TD(1, :)
-      y_out = TD_table(dIx)%TD(2, :)
-
-   end subroutine TD_get_data
+   end subroutine TD_get_td_from_file
 
 end module m_transport_data
